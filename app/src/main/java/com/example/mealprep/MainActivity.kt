a8be81220a293cf319bd44f7c5c165262fd5dbf5
Stephen-d-b <st10472491@vcconnect.edu.za>
2025-03-27 18:37:51 +0200

    Created a list of
    A list of funtion was created for the user to choose from  to enter and then a listof function was gave to each time for a random meal that can be gave for the user

--- app/src/main/java/com/example/mealprep/MainActivity.kt
+++ app/src/main/java/com/example/mealprep/MainActivity.kt
@@ -19,16 +19,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val txtInput=findViewById<EditText>(R.id.txtInput)
-        val btnGenMeal=findViewById<Button>(R.id.btnGenMeal)
-        val lblOutput=findViewById<TextView>(R.id.lblOutput)
-        val btnReset=findViewById<Button>(R.id.btnReset)
+        val txtInput = findViewById<EditText>(R.id.txtInput)
+        val btnGenMeal = findViewById<Button>(R.id.btnGenMeal)
+        val lblOutput = findViewById<TextView>(R.id.lblOutput)
+        val btnReset = findViewById<Button>(R.id.btnReset)
 
-        btnReset.setOnClickListener{
+        btnReset.setOnClickListener {
             txtInput.text.clear()
-            lblOutput.text=""
+            lblOutput.text = ""
         }
+        val timeOfDay =
+            listOf("Morning", "Mid-Morning", "Afternoon", "Mid-Afternoon", "Dinner") //https://kotlinlang.org//
 
-        
+        if (txtInput in timeOfDay) {
+            val mealOptions = mapOf(
+                "Morning" to listOf("Eggs", "Pancakes", "Oatmeal", "Toast & Jam"),
+                "Mid-Morning" to listOf("Yogurt", "Fruit Salad", "Granola Bar"),
+                "Afternoon" to listOf("Sandwich", "Salad", "Pasta", "Rice & Chicken"),
+                "Mid-Afternoon" to listOf("Nuts", "Smoothie", "Biscuits"),
+                "Dinner" to listOf("Steak", "Soup", "Pizza", "Grilled Fish")
+            )//https://kotlinlang.org//
+        }
+        btnGenMeal.setOnClickListener {
+            
+        }
     }
 }

de375cacbc2cedb81431d13bd69d4a733fbe33fb
Stephen-d-b <st10472491@vcconnect.edu.za>
2025-03-29 15:37:18 +0200

    Test app was made
    had a few errors with the  mapof functions trying to call the meals from the sertain list off function for each time of day

--- app/src/main/java/com/example/mealprep/MainActivity.kt
+++ app/src/main/java/com/example/mealprep/MainActivity.kt
@@ -31,17 +31,30 @@ class MainActivity : AppCompatActivity() {
         val timeOfDay =
             listOf("Morning", "Mid-Morning", "Afternoon", "Mid-Afternoon", "Dinner") //https://kotlinlang.org//
 
-        if (txtInput in timeOfDay) {
-            val mealOptions = mapOf(
-                "Morning" to listOf("Eggs", "Pancakes", "Oatmeal", "Toast & Jam"),
-                "Mid-Morning" to listOf("Yogurt", "Fruit Salad", "Granola Bar"),
-                "Afternoon" to listOf("Sandwich", "Salad", "Pasta", "Rice & Chicken"),
-                "Mid-Afternoon" to listOf("Nuts", "Smoothie", "Biscuits"),
-                "Dinner" to listOf("Steak", "Soup", "Pizza", "Grilled Fish")
-            )//https://kotlinlang.org//
-        }
         btnGenMeal.setOnClickListener {
-            
+            val userInput = txtInput.text.toString().trim()  // Get the input as a string
+
+            if (userInput in timeOfDay) {  // getting the user input from the variable called above
+                /* giving pre determinde meals for the the time of the day using the map of function to call random meals and
+                knowlage from the list of functions used in the weather app i made for ice task
+                trying to give them pre determinde if this does not work use basic if functions by just with "vaste uitkomste"  */
+                val mealOptions = mapOf(
+                    "Morning" to listOf("Eggs", "Pancakes", "Oatmeal", "Toast & Jam"),
+                    "Mid-Morning" to listOf("Yogurt", "Fruit Salad", "Granola Bar"),
+                    "Afternoon" to listOf("Sandwich", "Salad", "Pasta", "Rice & Chicken"),
+                    "Mid-Afternoon" to listOf("Nuts", "Smoothie", "Biscuits"),
+                    "Dinner" to listOf("Steak", "Soup", "Pizza", "Grilled Fish")
+                )//https://kotlinlang.org//
+
+                val Meels = mealOptions[userInput]
+
+                if (Meels != null) {
+                    lblOutput.text = "$userInput: $Meels"
+                } else {
+                    lblOutput.text = "You have typed an invalid option"
+                }
+            }
         }
     }
 }
+
